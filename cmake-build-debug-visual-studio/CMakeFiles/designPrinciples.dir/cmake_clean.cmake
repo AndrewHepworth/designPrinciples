cabbef8afde7ee0e99ae5f73924f24053e62c9ae
Andrew Hepworth <andy_hepworth95@yahoo.co.uk>
2021-10-28 11:28:49 +0100

    Init commit

---
+++ cmake-build-debug-visual-studio/CMakeFiles/designPrinciples.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/designPrinciples.dir/main.cpp.obj"
+  "designPrinciples.exe"
+  "designPrinciples.exe.manifest"
+  "designPrinciples.lib"
+  "designPrinciples.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/designPrinciples.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
